f1f007974f8b22c9cff91d2986bbd9c25f008c85
gmartpad <contato.gabrielmartinspadoin@gmail.com>
2025-05-11 12:54:39 -0300

    [NG] RESOLVING_ROUTE-RELATED_DYNAMIC_DATA - feat: implementing a resolver to get userName dynamically before the component assigned to the route resolves

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from "@angular/router";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
-import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
+import { resolveUserName, UserTasksComponent } from "./users/user-tasks/user-tasks.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
 import { routes as usersRoutes } from "./users/users.routes";
 
@@ -15,6 +15,9 @@ const routes: Routes = [
     children: usersRoutes,
     data: {
       message: 'Hello, World!'
+    },
+    resolve: {
+      userName: resolveUserName
     }
   }, 
   {
